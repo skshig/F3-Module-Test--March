7aa7459e0bd81c7ed34e3511977c6e9b12e179ba
skshig <skshig@gmail.com>
2023-04-04 18:03:29 +0530

    first two functions done

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 let picture = document.getElementById('current-image-container');
-const APIKEY = '4QLNcg7cxMVpOfjkrrC0yl4NgUSk9kmlT7xeMISK'
-const url = 'https://api.nasa.gov/planetary/apod?'
+let current_image = document.getElementById('current-picture')
+const APIKEY = '4QLNcg7cxMVpOfjkrrC0yl4NgUSk9kmlT7xeMISK';
+const apodUrl = 'https://api.nasa.gov/planetary/apod';
+let picture_detail = document.getElementById('pictureDetail');
 
-fetch('https://api.nasa.gov/planetary/apod?api_key=4QLNcg7cxMVpOfjkrrC0yl4NgUSk9kmlT7xeMISK')
-.then((response)=>{
-    return response.json()
-}).then((data)=>{
+function getCurrentImageOfTheDay(){
+    const currentDate = new Date().toISOString().split("T")[0];
+    fetch(`${apodUrl}?api_key=${APIKEY}&date=${currentDate}`)
+        .then((response)=>{
+            console.log(response)
+            return response.json()
 
-    console.log(data);
-})
+        }).then((data)=>{
+            picture_detail.innerHTML = `<h2>${data.title}</h2><p>${data.explanation}</p>`;
+            current_image.src = data.url;
 
-document.getElementById('btn')
+            })
+}
+ function getImageOfTheDay(){
+    const selctedDate = document.getElementById('search-input').value;
+    console.log("hi")
+    fetch(`${apodUrl}?api_key=${APIKEY}&date=${selctedDate}`)
+        .then((response)=>{
+            console.log(response)
+            return response.json()
+
+        }).then((data)=>{
+            picture_detail.innerHTML = `<h2>${data.title}</h2><p>${data.explanation}</p>`;
+            current_image.src = data.url;
+
+            })
+}
+
+// document.getElementById('btn').addEventListener('click',getImageOfTheDay());
